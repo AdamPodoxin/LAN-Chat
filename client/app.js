0705f941bd8aef4c6019e74b9c831ea1e611ea3a
Adam Pod <adam.podoxin@gmail.com>
2019-07-17 23:09:24 -0700

    Create messaging system

--- client/app.js
+++ client/app.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-	
+	var socket = io();
+
+	var name = "User";
+
+	$("#chat").submit(function() {
+		var msg = name + ": " + $("#message").val();
+		socket.emit("message", msg);
+
+		$("#message").val("");
+
+		return false;
+	});
+
+	socket.on("message", function(msg) {
+		$("<li>").text(msg).appendTo("#chat-list");
+	});
 });
